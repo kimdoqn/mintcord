b1ff55a47b19d1230b798cb63e9cf28401d90e43
kimdoqn <kimdoqn@gmail.com>
2020-01-21 12:57:41 +0000

    simple logging

--- mintcord-frontend/src/components/Chat/Chat.js
+++ mintcord-frontend/src/components/Chat/Chat.js
@@ -9,14 +9,23 @@ const Chat = () => {
 	
 	// TODO: apply redux, cuz we have to store message until send
 	const [ message, setMessage ] = useState('');
+	const [ messageLog, setMessageLog] = useState('Init');
 	const handleMessageChange = (event) => {
 		setMessage(event.target.value);
 	}
 
+	const onClickButton = () => {
+		setMessage('');
+		setMessageLog(messageLog + '\n' + message);
+	}
 	return (
 		<div className={cx('chat')}>
 			<div className={cx('chat-log')}>
-				chat log
+				{
+					messageLog.split('\n').map( line => {
+						return (<span>{line}<br/></span>)
+					})
+				}
 			</div>
 			<input
 				type='text'
@@ -24,7 +33,7 @@ const Chat = () => {
 				value={message}
 				onChange={handleMessageChange}
 			/>
-			<button>Send</button>
+			<button onClick={onClickButton}> Send </button>
 		</div>
 	);
 }
