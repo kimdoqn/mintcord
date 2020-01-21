77633ef4d07032fd641d4d791f012ba2c2166a1d
kimdoqn <kimdoqn@gmail.com>
2020-01-21 17:15:44 +0000

    div seperate logging

--- mintcord-frontend/src/components/Chat/Chat.js
+++ mintcord-frontend/src/components/Chat/Chat.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import styles from './Chat.scss';
+import MessageBox from 'components/Chat/MessageBox';
+
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
@@ -9,23 +11,32 @@ const Chat = () => {
 	
 	// TODO: apply redux, cuz we have to store message until send
 	const [ message, setMessage ] = useState('');
-	const [ messageLog, setMessageLog] = useState('Init');
+	const [ messageLog, setMessageLog] = useState([]);
 	const handleMessageChange = (event) => {
 		setMessage(event.target.value);
 	}
-
 	const onClickButton = () => {
 		setMessage('');
-		setMessageLog(messageLog + '\n' + message);
+		setMessageLog([
+			...messageLog,
+			{
+				id: Math.floor(Math.random() * 100),
+				txt: message
+			}
+		]);
 	}
+	
 	return (
 		<div className={cx('chat')}>
 			<div className={cx('chat-log')}>
-				{
-					messageLog.split('\n').map( line => {
-						return (<span>{line}<br/></span>)
-					})
-				}
+				{messageLog.map(msg => (
+					<MessageBox 
+						key={msg.id}
+						me={msg.id % 2}
+						user={msg.id}
+						message={msg.txt}
+						/>
+				))}
 			</div>
 			<input
 				type='text'
